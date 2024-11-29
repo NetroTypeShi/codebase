b7fb4eccdebba5eccb455583590883c425050b78
NetroTypeShi <andresmenduina@gmail.com>
2024-11-29 11:45:38 +0100

    Add files via upload

--- jsonbin-test/crud.py
+++ jsonbin-test/crud.py
@@ -15,8 +15,29 @@ XAccessKey = "$2a$10$beAyfN2I7J9Ziwkz6tBTc.E3zBa99mAoHKSGLpozscT8vXF8b5fKW"
 url_root = "https://api.jsonbin.io/v3"
 route = f"/b/{bin_id}"
 headers ={
+    'Content-Type': 'application/json',
     "X-Master-Key" : XMasterKey,
     "X-Access-Key" : XAccessKey
 }
-r = requests.get(url_root + route, headers=headers)
-print(r.json())
+usuario = {
+    "username": "gigga_nigga",
+    "password": "big_ass_nigga",
+    "mensaje": "I can't breathe"
+}
+
+def read_db():
+    r = requests.get(url_root + route, headers=headers)
+    print(r.json())
+
+def update_db():
+    r = requests.put(url_root + route, headers=headers, json=usuario)
+    print(r.json())
+
+def add_user():
+    r = requests.patch(url_root + route, headers=headers, json=usuario)
+    print(r.json())
+
+read_db()
+update_db()
+read_db()
+add_user()
