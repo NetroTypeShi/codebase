353354af4fb4f8b6e4f7724034d9602a3fa9a505
NetroTypeShi <andresmenduina@gmail.com>
2025-03-09 20:53:07 +0100

    Add files via upload

--- objetos-avanzados/ejercicioforEach.js
+++ objetos-avanzados/ejercicioforEach.js
@@ -3,6 +3,17 @@ const lista = ["manzana", "pera", "plátano", "cereza"]
 // usando listas y strings, devuelve un string que capitalice cada palabra y luego las junte en un sting
 //ej: ManzanaPeraPlatanoCereza
 
+let answer = "";
+
+lista.forEach(fruta => {
+  // Capitalizamos la primera letra y añadimos el resto de la palabra
+  const frutaCapitalizada = fruta[0].toUpperCase() + fruta.slice(1);
+  // Concatenamos la fruta capitalizada a la respuesta
+  answer += frutaCapitalizada;
+});
+
+console.log(answer); // Imprime: ManzanaPeraPlatanoCereza
+
 const string = "hola mundo"
 
 // usando metodos listas y strings, devuelve el anterior string
