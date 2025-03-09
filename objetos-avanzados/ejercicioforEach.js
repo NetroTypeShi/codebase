739f193d04c35df802383081072eb585ee642a29
NetroTypeShi <andresmenduina@gmail.com>
2025-03-09 20:52:02 +0100

    Add files via upload

---
+++ objetos-avanzados/ejercicioforEach.js
@@ -0,0 +1,11 @@
+const lista = ["manzana", "pera", "plátano", "cereza"]
+
+// usando listas y strings, devuelve un string que capitalice cada palabra y luego las junte en un sting
+//ej: ManzanaPeraPlatanoCereza
+
+const string = "hola mundo"
+
+// usando metodos listas y strings, devuelve el anterior string
+// alternar entre mayúsculas y minúsculas
+
+//EXTRA: Haz algo interesante con metodos de strings, arrays, numeros, whatever
